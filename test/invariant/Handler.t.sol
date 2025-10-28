35c2cea084d12a8e5651271747362d9df01f8da9
Patrick-Ngururi <patrick9ngururi8@gmail.com>
2025-10-28 21:52:34 +0300

    Setting up Handler.t.sol

---
+++ test/invariant/Handler.t.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import { Test, console2 } from "forge-std/Test.sol";
+import { TSwapPool } from "../../src/TSwapPool.sol";
+import { ERC20Mock } from "../mocks/ERC20Mock.sol";
+
+contract Handler is Test {
+    TSwapPool pool;
+    ERC20Mock weth;
+    ERC20Mock poolToken;
+
+    // Ghost Variables - variables that only exist in our Handler
+    int256 public expectedDeltaY;
+    int256 public expectedDeltaX;
+    int256 startingY;
+    int256 startingX;
+    int256 public actualDeltaX;
+    int256 public actualDeltaY;
+
+    constructor(TSwapPool _pool) {
+        pool = _pool;
+        weth = ERC20Mock(_pool.getWeth());
+        poolToken = ERC20Mock(_pool.getPoolToken());
+    }
+
+   function deposit(uint256 wethAmount) public {
+        wethAmount = bound(wethAmount, 0, type(uint64).max);
+        startingY = int256(weth.balanceOf(address(this)));
+        startingX = int256(poolToken.balanceOf(address(this)));
+        expectedDeltaX = int256(wethAmount);
+        expectedDeltaY = int256(pool.getPoolTokensToDepositBasedOnWeth(wethAmount));
+    
+        // Pranking LP and minting tokens/approving the pool
+        vm.startPrank(liquidityProvider);
+        weth.mint(liquidityProvider, wethAmount);
+        poolToken.mint(liquidityProvider, uint256(expectedDeltaX));
+        weth.approve(address(pool), type(uint256).max);
+        poolToken.approve(address(pool), type(uint256).max);
+
+        // Deposit
+        pool.deposit(wethAmount, 0, uint256(expectedDeltaX), uint64(block.timestamp));
+        vm.stopPrank();
+
+        // Check Actual Deltas
+        uint256 endingY = poolToken.balanceOf(address(this));
+        uint256 endingX = weth.balanceOf(address(this));
+        
+        actualDeltaY = int256(endingY) - int256(startingY);
+        actualDeltaX = int256(endingX) - int256(startingX);
+    }
+}
