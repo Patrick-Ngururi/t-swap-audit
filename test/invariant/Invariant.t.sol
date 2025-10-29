3f0a70f861d40b3b0f91f11b1e584d9a69200837
Patrick-Ngururi <patrick9ngururi8@gmail.com>
2025-10-30 00:10:40 +0300

    Final Invariant And Tweaks

--- test/invariant/Invariant.t.sol
+++ test/invariant/Invariant.t.sol
@@ -7,6 +7,7 @@ import { StdInvariant } from "forge-std/StdInvariant.sol";
 import { ERC20Mock } from "test/mocks/ERC20Mock.sol";
 import { PoolFactory } from "../../src/PoolFactory.sol";
 import { TSwapPool } from "../../src/TSwapPool.sol";
+import {Handler} from "./Handler.t.sol";
 
 contract Invariant is StdInvariant, Test {
     ERC20Mock poolToken;
@@ -14,6 +15,7 @@ contract Invariant is StdInvariant, Test {
 
     PoolFactory factory;
     TSwapPool pool; // poolToken, weth
+    Handler handler;
 
     int256 constant STARTING_X = 100e18; // starting ERC20 / poolToken
     int256 constant STARTING_Y = 50e18; // starting WETH
@@ -33,5 +35,16 @@ contract Invariant is StdInvariant, Test {
 
         // Deposit into Pool, gice the starting x & y balances
         pool.deposit(uint256(STARTING_Y), uint256(STARTING_Y), uint256(STARTING_X), uint64(block.timestamp));
+
+        handler = new Handler(pool);
+        bytes4[] memory selectors = new bytes4[](2);
+        selectors[0] = handler.deposit.selector;
+        selectors[1] = handler.swapPoolTokenForWethBasedOnOutputWeth.selector;
+        targetSelector(FuzzSelector({ addr: address(handler), selectors: selectors }));
+        targetContract(address(handler));
+    }
+
+    function statefulFuzz_constantProductFormulaStaysTheSameY() public view {
+        assertEq(handler.actualDeltaX(), handler.expectedDeltaY());
     }
 }
